c74154a455ca19a662ff2d896ddac62630d5f04a
david <david.hoyos@ceiba.com.co>
2017-11-06 16:45:22 -0500

    version4

--- src/main/java/org/gradle/ProcesoPrincipal.java
+++ src/main/java/org/gradle/ProcesoPrincipal.java
@@ -1,14 +1,18 @@
 package org.gradle;
 
+import java.util.logging.Logger;
+
 public class ProcesoPrincipal {
 
 	
 	public static void main(String[] args) {
 		
+		final  Logger LOGGER =Logger.getLogger(ProcesoPrincipal.class.getName());
+		
 	ServicioCalculadora servicio = new ServicioCalculadora();
 	int res = servicio.sumar(4, 3);
-	System.out.println("El resultadoe es ");
-	System.out.println(res);
+	
+	LOGGER.info("El resultado es " + String.valueOf(res));
 		
 	}
 
